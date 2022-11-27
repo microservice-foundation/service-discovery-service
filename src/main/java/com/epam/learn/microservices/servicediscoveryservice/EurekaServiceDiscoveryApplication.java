3123e2778369db2494716962cd06ab2ca95cacc4
yusuf-murodov-epam <yusuf_murodov@epam.com>
2022-11-28 00:05:47 +0500

    initial commit

---
+++ src/main/java/com/epam/learn/microservices/servicediscoveryservice/EurekaServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.epam.learn.microservices.servicediscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceDiscoveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceDiscoveryApplication.class, args);
+    }
+}
